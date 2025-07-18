f0d085a53bdcbabb7baf00733306f3cfb7ab4141
Enzo Lee <soloenzo.dev@gmail.com>
2025-07-18 17:23:30 +0700

    Add ProjectSidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ProjectSidebar from './ProjectSidebar'
+
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
+      <ProjectSidebar></ProjectSidebar>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/ProjectSidebar.jsx
@@ -0,0 +1,10 @@
+export default function ProjectSidebar() {
+  return (
+    <aside className="project-sidebar">
+      <h2>Your Project</h2>
+      <div>
+        <button>+ Add Project</button>
+      </div>
+    </aside>
+  )
+}
